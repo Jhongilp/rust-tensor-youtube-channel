82e0b585819ac285b79bd7a74aea73ab3d461e74
Jonathan Gil <jhongilp@gmail.com>
2020-07-13 19:24:38 -0500

    add another example using shapes

--- README.md
+++ README.md
@@ -68,3 +68,33 @@ impl Keys {
     }
 }
 ```
+
+More example of enum using strucs and impl:
+
+```
+enum Shape {
+    Rectangle {width: u32, height: u32},
+    Square(u32),
+    Circle(f64),
+}
+
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Rectangle {width, height} => (width * height) as f64,
+            Shape::Square(s) => (s * s) as f64,
+            Shape::Circle(r) => 3.14 * (r * r),
+        }
+    }
+}
+
+fn main() {
+    let r = Shape::Rectangle{width: 10, height:70};
+    let s = Shape::Square(20);
+    let c = Shape::Circle(50.0);
+
+    let ar = r.area();
+    print!("rectangle area: {}", ar);
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,24 @@ impl Keys {
     }
 }
 
+
+enum Shape {
+    Rectangle {width: u32, height: u32},
+    Square(u32),
+    Circle(f64),
+}
+
+
+impl Shape {
+    fn area(&self) -> f64 {
+        match self {
+            Shape::Rectangle {width, height} => (width * height) as f64,
+            Shape::Square(s) => (s * s) as f64,
+            Shape::Circle(r) => 3.14 * (r * r),
+        }
+    }
+}
+
 fn main() {
     let u = Direccion::Up(Point { x: 0, y: 10 });
 
@@ -53,4 +71,11 @@ fn main() {
     let up_s = up.destruct();
     println!("{:?}", up);
     println!("{:?}", up_s);
+
+    let r = Shape::Rectangle{width: 10, height:70};
+    let s = Shape::Square(20);
+    let c = Shape::Circle(50.0);
+
+    let ar = r.area();
+    print!("rectangle area: {}", ar);
 }
