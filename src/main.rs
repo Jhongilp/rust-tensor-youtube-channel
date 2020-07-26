b13f6c8a70f9254e8f41681017dd3688fc80bafb
Jonathan Gil <jhongilp@gmail.com>
2020-07-26 08:24:27 -0500

    intro to smart pointers using Box

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,21 @@ enum List {
 use List::Cons;
 use List::End;
 
+
 fn main() {
     let b = Box::new(10);
     println!("B {}", b);
 
     let l = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(End))))));
     println!("l: {:?}", l);
+
+    // using Box as reference
+    let y = 4;
+    let x = &y;
+    let z = Box::new(y);
+
+    if *x == *z {
+        println!("x a z have the same pointer");
+    }
+
 }
